d83949f39d2cee46b6f3e9bcb8d16dc87600000b
zhangxueyan <1632895066@qq.com>
2018-03-06 15:11:11 +0800

    'update'

---
+++ yd-s52/js/basic.js
@@ -0,0 +1,45 @@
+﻿$(function(){
+    var obj = {
+      //Android Soft keyboard positioning
+      hideKeyboard:function(){
+       var wh=$(window).height();
+       $(window).resize(function() {
+        if($(window).height()<wh){
+          $('.fot-scroll').hide();
+        }else{
+          $('.fot-scroll').hide();
+        }
+       });
+      },
+      //updateTime
+      updateTime:function(){
+        var $year = $(".year");
+        var $month = $(".month");
+        var $date = $(".date");
+        var myDate = new Date();
+        var getDate = myDate.getDate();
+        var getDay = myDate.getDay();
+        switch(getDay){
+          case 0:myDate.setDate(getDate+3);
+          break;
+          case 1:myDate.setDate(getDate+2)
+          break;
+          case 2:myDate.setDate(getDate+1)
+          break;
+          case 3:myDate.setDate(getDate+2)
+          break;
+          case 4:myDate.setDate(getDate+1)
+          break;
+          case 5:myDate.setDate(getDate+2)
+          break;
+          case 6:myDate.setDate(getDate+1)
+          break;
+        }
+        $year.text(myDate.getFullYear());
+        $month.text(myDate.getMonth()+1);
+        $date.text(myDate.getDate());
+      }
+  }
+  obj.hideKeyboard()
+  obj.updateTime()
+})
